9efcd045a85cb1e0baa90447f27bc667f96c63bb
DANIEL STEVEN LÓPEZ HERNÁNDEZ <daniel2006503@outlook.com>
2025-03-20 08:31:46 -0300

    Se modifico el controlador de usuario

--- DSLH20250320.AppWebMVC/Controllers/UsersController.cs
+++ DSLH20250320.AppWebMVC/Controllers/UsersController.cs
@@ -1,14 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DSLH20250320.AppWebMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
 
 namespace DSLH20250320.AppWebMVC.Controllers
 {
+
+    //[Authorize(Roles = "ADMINISTRADOR")]
+
     public class UsersController : Controller
     {
         private readonly Test20250320DbContext _context;
@@ -57,12 +63,62 @@ namespace DSLH20250320.AppWebMVC.Controllers
         {
             if (ModelState.IsValid)
             {
+                user.PasswordHash = CalcularHashMD5(user.PasswordHash);
                 _context.Add(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(user);
         }
+
+
+
+
+
+
+        public async Task<IActionResult> CerrarSession()
+        {
+            // Hola mundo
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(User usuario)
+        {
+            usuario.PasswordHash = CalcularHashMD5(usuario.PasswordHash);
+            var usuarioAuth = await _context.
+                Users.
+                FirstOrDefaultAsync(s => s.Email == usuario.Email && s.PasswordHash == usuario.PasswordHash);
+            if (usuarioAuth != null && usuarioAuth.UserId > 0 && usuarioAuth.Email == usuario.Email)
+            {
+                var claims = new[] {
+                    new Claim(ClaimTypes.Name, usuarioAuth.Email),
+                    new Claim("UserId", usuarioAuth.UserId.ToString()),
+                     new Claim("Username", usuarioAuth.Username),
+                    new Claim(ClaimTypes.Role, usuarioAuth.Role)
+                    };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "El email o contraseña estan incorrectos");
+                return View();
+            }
+        }
+
+
+
+
+
+
 
         // GET: Users/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -91,14 +147,18 @@ namespace DSLH20250320.AppWebMVC.Controllers
             {
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
+           var usuarioUpdate = await _context.Users
+               .FirstOrDefaultAsync(m => m.UserId == user.UserId);
+            try
             {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
+                usuarioUpdate.Username = user.Username;
+                usuarioUpdate.Email = user.Email;
+                usuarioUpdate.Status = user.Status;
+                usuarioUpdate.Role = user.Role;
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!UserExists(user.UserId))
@@ -107,12 +167,9 @@ namespace DSLH20250320.AppWebMVC.Controllers
                     }
                     else
                     {
-                        throw;
-                    }
+                    return View(user);
                 }
-                return RedirectToAction(nameof(Index));
             }
-            return View(user);
         }
 
         // GET: Users/Delete/5
@@ -151,6 +208,61 @@ namespace DSLH20250320.AppWebMVC.Controllers
         private bool UserExists(int id)
         {
             return _context.Users.Any(e => e.UserId == id);
+        }
+        public async Task<IActionResult> Perfil()
+        {
+
+            var idStr = User.FindFirst("Id")?.Value;
+            int id = int.Parse(idStr);
+            var usuario = await _context.Users.FindAsync(id);
+            return View(usuario);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Perfil(int id, [Bind("Id,Nombre,Email,Estatus,Rol")] User usuario)
+        {
+            if (id != usuario.UserId)
+            {
+                return NotFound();
+            }
+            var usuarioUpdate = await _context.Users
+                 .FirstOrDefaultAsync(m => m.UserId == usuario.UserId);
+            try
+            {
+                usuarioUpdate.Username = usuario.Username;
+                usuarioUpdate.Email = usuario.Email;
+                usuarioUpdate.Status = usuario.Status;
+                usuarioUpdate.Role = usuario.Role;
+                _context.Update(usuarioUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(usuario.UserId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(usuario);
+                }
+            }
+        }
+        private string CalcularHashMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); // "x2" convierte el byte en una cadena hexadecimal de dos caracteres.
+                }
+                return sb.ToString();
+            }
         }
     }
 }
