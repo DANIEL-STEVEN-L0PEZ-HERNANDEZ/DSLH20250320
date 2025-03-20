e8ee048f96a36a9821cd1d4fc1a6837fe37142be
DANIEL STEVEN LÓPEZ HERNÁNDEZ <daniel2006503@outlook.com>
2025-03-20 08:36:57 -0300

    Se modifico el modelo de Usuario

--- DSLH20250320.AppWebMVC/Models/User.cs
+++ DSLH20250320.AppWebMVC/Models/User.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.NetworkInformation;
 
 namespace DSLH20250320.AppWebMVC.Models;
 
@@ -7,13 +10,48 @@ public partial class User
 {
     public int UserId { get; set; }
 
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Username { get; set; } = null!;
 
+
+    [Required(ErrorMessage = "El Email es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
+
+    [Required(ErrorMessage = "El password es obligatorio.")]
+    [DataType(DataType.Password)]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El passowrd debe tener entre 5 y 50 caracteres.")]
     public string PasswordHash { get; set; } = null!;
+
 
     public string Role { get; set; } = null!;
 
+
     public byte? Status { get; set; }
+    private string strEstatus;
+    [NotMapped]
+    public string StrEstatus
+    {
+        get
+        {
+            if (Status == 1)
+                strEstatus = "Activo";
+            else if (Status == 2)
+                strEstatus = "Inactivo";
+            return strEstatus;
+        }
+        set { strEstatus = value; }
+    }
+
+
+    [NotMapped]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El password debe tener entre 5 y 50 caracteres.")]
+    [Display(Name = "Confirmar Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+    public string? ConfirmarPassword { get; set; } = null!;
 }
+
+
