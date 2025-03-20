bf71e5cad58da61c9660d705a63fd5b92063e381
DANIEL STEVEN LÓPEZ HERNÁNDEZ <daniel2006503@outlook.com>
2025-03-20 07:59:34 -0300

    Se creo la DBContext con las tablas de base datos

---
+++ DSLH20250320.AppWebMVC/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSLH20250320.AppWebMVC.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public byte? Status { get; set; }
+}
